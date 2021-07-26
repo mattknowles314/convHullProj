0ef726fdd0aa5e197c079f274846dfd77011b1c2
Matthew Knowles <mattknowles314@gmail.com>
2021-07-26 14:28:27 +0100

    Tidying up

--- Code/mono_chain.py
+++ Code/mono_chain.py
@@ -1,48 +1,19 @@
 #Andrew's Monotone Chain Convex Hull
-import sorting
 import matplotlib.pyplot as plt
-
-S = [[2,1],
-[4,61],
-[6,44],
-[8,25],
-[10,89],
-[12,26],
-[14,12],
-[16,87],
-[18,79],
-[20,74],
-[22,22],
-[24,87],
-[26,49],
-[28,8],
-[30,9],
-[32,58],
-[34,6],
-[36,82],
-[38,4],
-[40,358],
-[42,198],
-[44,84],
-[46,53],
-[48,51],
-[50,87],
-[52,86],
-[54,20],
-[56,49]]
-L = []
-U = []
 
 def cross_product(o,a,b):
 	return (a[0]-o[0])*(b[1]-o[1]) - (a[1]-o[1])*(b[0]-o[0])
 
-if len(S) <= 3:
-	print("Error - Need more than 3 points")
-else:
-	for i in S:
-		while len(L) >= 2 and cross_product(L[-2], L[-1], i) <= 0:
-			L.pop()
-		L.append(i)
+def mono_conv_hull(S):
+	L = []
+	U = []
+	if len(S) <= 3:
+		print("Error - Need more than 3 points")
+	else:
+		for i in S:
+			while len(L) >= 2 and cross_product(L[-2], L[-1], i) <= 0:
+				L.pop()
+			L.append(i)
 '''
 sX = []
 sY = []
--- Code/rouxHull.py
+++ Code/rouxHull.py
@@ -1,77 +1,18 @@
 import matplotlib.pyplot as plt
-
-S=[[2,50],
-[4,61],
-[6,44],
-[8,25],
-[10,89],
-[12,26],
-[14,12],
-[16,87],
-[18,79],
-[20,74],
-[22,22],
-[23,1],
-[24,87],
-[26,49],
-[28,8],
-[30,9],
-[32,58],
-[34,6],
-[36,82],
-[38,4],
-[40,358],
-[42,198],
-[44,84],
-[46,53],
-[48,51],
-[50,87],
-[52,86],
-[54,20],
-[56,49]]
-hull = [S[0]]
+import time
+import random
+import statistics as st
 
 def grad(x0,x1,y0,y1):
 	return (y1-y0)/(x1-x0)
 
-def get_min(S):
-	min_index = 0
-	min_val = 100
-	for i in range(0, len(S)-1):
-		if S[i][1] <= min_val:
-			min_val = S[i][1]
-			min_index = i
-	return min_index
-
-def conv_hull(S):
+def roux_conv_hull(S):
+	hull = [S[0]]
 	curr_point = 0
 	while S[-1] not in hull:
 		grads = []
 		for i in range(curr_point, len(S)-1):
 			grads.append(grad(S[curr_point][0],S[i+1][0],S[curr_point][1],S[i+1][1]))
-		
-
 		m = grads.index(min(grads))
 		hull.append(S[curr_point+1+m])
 		curr_point = curr_point+1+m
-
-
-
-conv_hull(S)
-
-sX = []
-sY = []
-lX = []
-lY = []
-
-for i in S:
-	sX.append(i[0])
-	sY.append(i[1])
-for i in hull:
-	lX.append(i[0])
-	lY.append(i[1])
-
-plt.plot(sX,sY, color="blue", label="f(x)")
-plt.plot(lX,lY, '--r', label="CHull(f(x))")
-plt.legend(loc="upper left")
-plt.show()
