797d147b03a03d19b6720722b9cddd2dc8069c79
Matthew Knowles <mattknowles314@gmail.com>
2021-07-26 14:28:36 +0100

    Tidying up

--- Code/test_performance.py
+++ Code/test_performance.py
@@ -7,33 +7,37 @@ import time
 import os
 import matplotlib.pyplot as plt
 import statistics as st
+from rouxHull import *
+from mono_chain import *
 
-times = []
+lengths = []
+avg_times_roux = []
+avg_times_mono = []
+def test_hull(q,n):
+	S = []
+	for i in range(1,q):
+		p = [i,random.randint(1,200)]
+		S.append(p)
+	times_roux = []
+	times_mono = []
+	for i in range(0,n):	
+		t0 = time.time()
+		roux_conv_hull(S)
+		t1 = time.time()
+		mono_conv_hull(S)
+		t2 = time.time()
+		times_roux.append(t1-t0)
+		times_mono.append(t2-t1)
+	avg_times_roux.append(st.mean(times_roux))
+	avg_times_mono.append(st.mean(times_mono))
 
-def test_time():
-	t0 = time.time()
-	os.system("python3 mono_chain.py")
-	t1 = time.time()
-	times.append(t1-t0)
+for q in range(5,500):
+	lengths.append(q)
+	test_hull(q,50)
 
-def plot_times(avg_time): 
-	plt.plot(times)
-	plt.axhline(avg_time, color='r')
-	plt.xlabel('Run')
-	plt.ylabel('Time (s)')
-	plt.show()
-
-for i in range(0,100):
-	test_time()
-
-avg_time = st.mean(times)
-sd_time = st.stdev(times)
-print(avg_time, sd_time)
-plot_times(avg_time)
-#print(times)
-
-'''
-with open("../Data/piecewise.txt", "w") as file:
-	for time in times:
-		file.write("%f\n" % time)
-'''
+plt.plot(lengths,avg_times_roux, color="blue", label="RouxHull")
+plt.plot(lengths,avg_times_mono, color="red", label="Monotone-Chain")
+plt.legend(loc="upper left")
+plt.xlabel('Number of Points')
+plt.ylabel('Time (s)')
+plt.show()
