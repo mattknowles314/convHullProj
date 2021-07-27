34bad145552cbf9eb6759e4e97f1e9c287de3532
Matthew Knowles <mattknowles314@gmail.com>
2021-07-27 09:17:07 +0100

    Tidying up

--- Code/mono_chain.py
+++ Code/mono_chain.py
@@ -1,12 +1,8 @@
-#Andrew's Monotone Chain Convex Hull
-import matplotlib.pyplot as plt
-
 def cross_product(o,a,b):
 	return (a[0]-o[0])*(b[1]-o[1]) - (a[1]-o[1])*(b[0]-o[0])
 
 def mono_conv_hull(S):
 	L = []
-	U = []
 	if len(S) <= 3:
 		print("Error - Need more than 3 points")
 	else:
@@ -14,21 +10,3 @@ def mono_conv_hull(S):
 			while len(L) >= 2 and cross_product(L[-2], L[-1], i) <= 0:
 				L.pop()
 			L.append(i)
-'''
-sX = []
-sY = []
-lX = []
-lY = []
-
-for i in S:
-	sX.append(i[0])
-	sY.append(i[1])
-for i in L:
-	lX.append(i[0])
-	lY.append(i[1])
-
-plt.plot(sX,sY, color="blue", label="f(x)")
-plt.plot(lX,lY, '--r', label="CHull(f(x))")
-plt.legend(loc="upper left")
-plt.show()
-'''
--- Code/quickhull.py
+++ Code/quickhull.py
@@ -2,4 +2,7 @@ import matplotlib.pyplot as plt
 
 S = [[3,4],[4,7],[5,6],[6,7],[7,2]]
 
-print(S[0][0:len(S)-1])
+
+def QuickHull(S):
+	hull = []
+	hull.append(S[0])
